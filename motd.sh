95732162152d2b7ea88fbd0bce1107ee3593bc8f
tabletenniser <tabletenniser@gmail.com>
2019-04-28 18:27:19 -0700

    Add color to motd.sh

--- motd.sh
+++ motd.sh
@@ -1,13 +1,22 @@
 #/bin/sh
 
-echo "
-        |         Welcome aboard captain $(whoami)!
-       / \        Today is $(date), we are $(uptime -p).
+TOTAL_MEM=$(free -mht | command grep Total: | awk '{print $2}')
+FREE_MEM=$(free -mht | command grep Total: | awk '{print $4}')
+
+DARKGREY="$(tput sgr0 ; tput bold ; tput setaf 0)"
+RED="$(tput sgr0 ; tput setaf 1)"
+GREEN="$(tput sgr0 ; tput setaf 2)"
+BLUE="$(tput sgr0 ; tput setaf 4)"
+NC="$(tput sgr0)" # No Color
+
+echo "${GREEN}
+        |         Welcome aboard captain ${RED}$(whoami)!${GREEN}
+       / \        Today is ${RED}$(date)${GREEN}, we are ${RED}$(uptime -p)${GREEN}.
       / _ \       
      |.o '.|	  Space vessel computer:
-     |'._.'|     	$(uptime | command grep -ohe 'load average[s:][: ].*') 
-     |     |       	Disk: $(df -hT / | command grep /)
-   ,'|  |  |'.     	Memory Total: $(free -mht | command grep Total: | awk '{print $2}'); Free: $(free -mht | command grep Total: | awk '{print $4}')
-  /  |  |  |  \    	IP address: $(ip route | command grep default | awk '{print $7}')
-  |,-'--|--'-.|    	Gateway: $(ip route | command grep default | awk '{print $3}')
-"
+     |'._.'|     	Load: ${RED}$(uptime | command grep -ohe 'load average[s:][: ].*' | cut -d ':' -f2)${GREEN}
+     |     |       	Disk: ${RED}$(df -hT / | command grep /)${GREEN}
+   ,'|  |  |'.     	Memory Total: ${RED}${TOTAL_MEM}${GREEN}; Free: ${RED}${FREE_MEM}${GREEN}
+  /  |  |  |  \    	IP address: ${RED}$(ip route | command grep default | awk '{print $7}')${GREEN}
+  |,-'--|--'-.|    	Gateway: ${RED}$(ip route | command grep default | awk '{print $3}')${DARKGREY}
+${NR}"
