0e890fc0014d9f9687e38d582220e6c469246451
tabletenniser <tabletenniser@gmail.com>
2021-12-28 01:51:06 -0800

    Monitor training balls from Paddle Palace website

--- monitor_and_alert_mac.sh
+++ monitor_and_alert_mac.sh
@@ -1,11 +1,11 @@
 # Useful shell script that alerts when a open spot is available.
 while :;
 do clear;
-  curl https://www.cvs.com/vaccine/intake/ 2>/dev/null | /usr/bin/grep "We are adding"
-  if curl https://www.cvs.com/vaccine/intake/ 2>/dev/null | /usr/bin/grep "We are adding"; then
-    say "No vaccine appointment!!!"
+  curl https://www.paddlepalace.com/Nittaku-J-Top-40-Training-Ball-Bulk-Pack/productinfo/BNAJK/WHITE/ 2>/dev/null | /usr/bin/grep "10-Dozen White" | grep "Out"
+  if curl https://www.paddlepalace.com/Nittaku-J-Top-40-Training-Ball-Bulk-Pack/productinfo/BNAJK/WHITE/ 2>/dev/null | /usr/bin/grep "10-Dozen White" | grep "In Stock"; then
+    say "Training balls are in stock!!!"
   else
-    say "Vaccine appointment available!!!"
+    echo "Training balls are out of stock!!!"
   fi
   sleep 30;
 done
